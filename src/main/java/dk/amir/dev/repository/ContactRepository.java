cc81053810654f0ade8767dff7af9bd689baf809
amirholya01 <amirholya01@gmail.com>
2025-05-21 06:53:31 +0200

    making repository

---
+++ src/main/java/dk/amir/dev/repository/ContactRepository.java
@@ -0,0 +1,13 @@
+package dk.amir.dev.repository;
+
+import dk.amir.dev.model.entity.Contact;
+import dk.amir.dev.model.enums.ContactType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Integer> {
+    List<Contact> findByType(ContactType type);
+}
