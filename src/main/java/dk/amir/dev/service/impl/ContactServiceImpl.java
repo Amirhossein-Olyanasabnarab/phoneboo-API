7a79e4471b825287f8f5fd67af71e607faf5dc84
amirholya01 <amirholya01@gmail.com>
2025-05-21 07:25:04 +0200

    create, getAll contact service

--- src/main/java/dk/amir/dev/service/impl/ContactServiceImpl.java
+++ src/main/java/dk/amir/dev/service/impl/ContactServiceImpl.java
@@ -2,21 +2,43 @@ package dk.amir.dev.service.impl;
 
 import dk.amir.dev.dto.BusinessContactDto;
 import dk.amir.dev.dto.PersonalContactDto;
+import dk.amir.dev.model.entity.BusinessContact;
+import dk.amir.dev.model.entity.PersonalContact;
+import dk.amir.dev.repository.ContactRepository;
 import dk.amir.dev.service.ContactService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ContactServiceImpl implements ContactService {
+
+    private final ContactRepository repository;
+    private final ModelMapper mapper;
+
+    @Autowired
+    public ContactServiceImpl(ContactRepository repository, ModelMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+
+
     @Override
     public PersonalContactDto createPersonalContact(PersonalContactDto dto) {
-        return null;
+
+        PersonalContact contact = mapper.map(dto, PersonalContact.class);
+        return mapper.map(repository.save(contact), PersonalContactDto.class);
     }
 
     @Override
     public BusinessContactDto createBusinessContact(BusinessContactDto dto) {
-        return null;
+
+        BusinessContact contact = mapper.map(dto, BusinessContact.class);
+        return mapper.map(repository.save(contact), BusinessContactDto.class);
     }
 
     @Override
@@ -31,12 +53,22 @@ public class ContactServiceImpl implements ContactService {
 
     @Override
     public List<PersonalContactDto> getPersonalContacts() {
-        return List.of();
+
+        return repository.findAll()
+                .stream()
+                .filter(c -> c instanceof  PersonalContact)
+                .map(c -> mapper.map(c, PersonalContactDto.class))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<BusinessContactDto> getBusinessContacts() {
-        return List.of();
+
+        return repository.findAll()
+                .stream()
+                .filter(c -> c instanceof BusinessContact)
+                .map(c -> mapper.map(c, BusinessContactDto.class))
+                .collect(Collectors.toList());
     }
 
     @Override
