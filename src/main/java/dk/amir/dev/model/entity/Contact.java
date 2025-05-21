342575a92ca68e33946627f4214411caff4b2c3a
amirholya01 <amirholya01@gmail.com>
2025-05-21 06:46:53 +0200

    refactoring base entity class and application properties, installed postgresql, jpa

--- src/main/java/dk/amir/dev/model/entity/Contact.java
+++ src/main/java/dk/amir/dev/model/entity/Contact.java
@@ -1,13 +1,39 @@
 package dk.amir.dev.model.entity;
 
 import dk.amir.dev.model.enums.ContactType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "type")
 public abstract class Contact {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 150)
     private String fullName;
+
+    @Column(nullable = false)
     private String phoneNumber;
     private String email;
-    private ContactType contactType;
-    private Boolean deleted;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", insertable = false, updatable = false)
+    private ContactType type;
+    private Boolean deleted = false;
+
+
+    public Contact(String fullName, String phoneNumber, String email) {
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
 }
