d13a5958253acb23694248275acae8a61b56c193
amirholya01 <amirholya01@gmail.com>
2025-06-10 07:57:20 +0200

    Refactoring ContactService Interface

--- src/main/java/dk/amir/dev/service/ContactService.java
+++ src/main/java/dk/amir/dev/service/ContactService.java
@@ -1,25 +1,15 @@
 package dk.amir.dev.service;
 
-import dk.amir.dev.dto.BusinessContactDto;
-import dk.amir.dev.dto.PersonalContactDto;
+import dk.amir.dev.model.entity.Contact;
 
 import java.util.List;
 
 public interface ContactService {
 
-    PersonalContactDto createPersonalContact(PersonalContactDto dto);
-    BusinessContactDto createBusinessContact(BusinessContactDto dto);
-
-    PersonalContactDto getPersonalContactById(Long id);
-    BusinessContactDto getBusinessContactById(Long id);
-
-    List<PersonalContactDto> getPersonalContacts();
-    List<BusinessContactDto> getBusinessContacts();
-
-    PersonalContactDto updatePersonalContact(Long id, PersonalContactDto dto);
-    BusinessContactDto updateBusinessContact(Long id, BusinessContactDto dto);
-
-    boolean deletePersonalContact(Long id);
-
+    Contact addContact(Contact contact);
+    Contact updateContact(Long id, Contact contact);
+    boolean deleteContact(Long id);
+    List<Contact> getAllContacts();
+    Contact getContactById(Long id);
 
 }
